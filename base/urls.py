cf0af9b1957d9074fe91de7cf27e8147ed9dfd42
pere49 <perekevin49@gmail.com>
2024-12-18 12:43:39 +0300

    configuring django rest framework for api

--- base/urls.py
+++ base/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
     path('register/', views.registerPage, name="register"),
+    path('update-user/<str:pk>', views.updateUser, name="update-user"),
 
     path('', views.home, name="home"),
     path('room/<str:pk>', views.room, name="room"),
@@ -14,4 +15,7 @@ urlpatterns = [
     path('update-room/<str:pk>', views.updateRoom, name="update-room"),
     path('delete-room/<str:pk>', views.deleteRoom, name="delete-room"),
     path('delete-message/<str:pk>', views.deleteMessage, name="delete-message"),
+
+    path('topics/', views.topicsPage, name="topics"),
+    path('activity/', views.activityPage, name="activity"),
 ]
